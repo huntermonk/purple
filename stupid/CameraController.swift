75bfed217e027e93564185e059ab2f0fc1a3cb7f
Hunter Maximillion Monk <hunter.monk@gmail.com>
2017-12-16 14:03:10 -0800

    Beginning of Sticker static vars

--- stupid/CameraController.swift
+++ stupid/CameraController.swift
@@ -25,10 +25,6 @@ class CameraController: UIViewController {
         sceneView.session.pause()
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        addAtHit(touches: touches)
-    }
-
     private func addAtHit(touches: Set<UITouch>) {
         addAnchor(distance: 2, camera: sceneView.session.currentFrame!.camera.transform)
         sceneView.removeAnchors()
@@ -41,6 +37,97 @@ class CameraController: UIViewController {
 
         let anchor = ARAnchor(transform: transform)
         sceneView.session.add(anchor: anchor)
+    }
+
+    private func addButton() {
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "sticker"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.constrain(size: CGSize(width: 40, height: 40))
+        button.pinTopMargin(constant: 16)
+        button.pinRight(constant: -16)
+        button.showsTouchWhenHighlighted = true
+        button.addTarget(self, action: #selector(showStickers), for: .touchUpInside)
+    }
+
+    @objc private func showStickers() {
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        addAtHit(touches: touches)
+    }
+}
+
+final class StickersTableController: UITableViewController {
+
+    var stickers: [Sticker] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup(table: tableView)
+        title = "Stickers"
+    }
+
+    private func setup(table: UITableView) {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.separatorEffect = UIVibrancyEffect(blurEffect: UIBlurEffect(style: .light))
+        tableView.backgroundColor = .clear
+
+        let size = CGSize(width: 250, height: 400)
+        tableView.frame = CGRect(origin: CGPoint.zero, size: size)
+        preferredContentSize = size
+    }
+}
+
+struct Sticker {
+    var node: SCNNode
+    var keyword: String
+    var title: String
+
+    static var angel: Sticker {
+        return Sticker(node: SCNNode(), keyword: "angel wings", title: "😇")
+    }
+
+    static var brazzers: Sticker {
+        return Sticker(node: SCNNode.brazzers, keyword: "brazzers", title: "😈")
+    }
+
+    static var kanyeGlasses: Sticker {
+        return Sticker(node: SCNNode.brazzers, keyword: "kanye west glasses", title: "🐐")
+    }
+
+    static var mustache: Sticker {
+        return Sticker(node: SCNNode.brazzers, keyword: "mustache", title: "👨🏽")
+    }
+
+    static var sunglasses: Sticker {
+        return Sticker(node: SCNNode.brazzers, keyword: "glasses, deal with it", title: "🕶")
+    }
+
+}
+
+// MARK: - Table view data source
+extension StickersTableController {
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sticker = stickers[indexPath.row]
+        print(sticker.title)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stickers.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = stickers[indexPath.row].title
+        return cell
     }
 
 }
