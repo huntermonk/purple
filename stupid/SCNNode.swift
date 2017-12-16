939da4422406c98891c009dca80cb45b120b4a35
Hunter Maximillion Monk <hunter.monk@gmail.com>
2017-12-16 14:11:00 -0800

    Add sticker SCNNodes

--- stupid/SCNNode.swift
+++ stupid/SCNNode.swift
@@ -7,8 +7,24 @@ extension SCNNode {
         return SCNNode(geometry: SCNSphere(radius: 0.01))
     }
 
+    static var angelWings: SCNNode {
+        return node(image: UIImage(named: "angel-wings")!, width: 3.452, height: 2.558)
+    }
+
     static var brazzers: SCNNode {
         return node(image: #imageLiteral(resourceName: "brazzers"), width: 0.35, height: 0.082)
+    }
+
+    static var kanyeGlasses: SCNNode {
+        return node(image: #imageLiteral(resourceName: "kanye-glasses"), width: 0.2, height: 0.2)
+    }
+
+    static var mustache: SCNNode {
+        return node(image: #imageLiteral(resourceName: "mustache"), width: 0.256, height: 0.128)
+    }
+
+    static var sunglasses: SCNNode {
+        return node(image: #imageLiteral(resourceName: "sunglasses"), width: 0.2048, height: 0.0456)
     }
 
     static func node(image: UIImage, width: CGFloat, height: CGFloat) -> SCNNode {
